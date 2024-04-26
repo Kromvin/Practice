f255f9379d241f0a5c5270fc73f5725f868db916
Kromvin <131769841+Kromvin@users.noreply.github.com>
2024-04-26 11:45:42 +0500

    Delete резерв directory

--- резерв/includes/cone.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "Krom", "");
-if ($conn === false) {
-  die("Ошибка: " . mysqli_connect_error());
-} 
-echo "Подключение успешно установлено";
-mysqli_close($conn);
-?>
--- резерв/includes/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require("constant.php");
-	$conn = mysqli_connect("localhost","Krom", "") or die(mysqli_connect_error());
-	mysqli_select_db ( $conn , "userlistdb" ) or die("Cannot select DB");
-?>.
