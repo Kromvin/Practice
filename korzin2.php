339033da1b83f56b9a8ca49ffcd566d8734179b4
Kromvin <131769841+Kromvin@users.noreply.github.com>
2024-04-26 12:10:52 +0500

    Update korzin2.php

--- korzin2.php
+++ korzin2.php
@@ -39,11 +39,11 @@ else:
       <link href="css/index6.css" rel="stylesheet" />
       <div class="cart1440-container">
         <div class="cart1440-cart1440">
-          <img src="public6/external/rectangle821625-omis-200h.png" alt="Rectangle821625" class="cart1440-rectangle82" />
+          <img src="pbl/public6/external/rectangle821625-omis-200h.png" alt="Rectangle821625" class="cart1440-rectangle82" />
           <div class="cart1440-group341">
             <div class="cart1440-group338">
               <div class="cart1440-group281">
-                <img src="public6/external/ellipse101727-wzz8-200h.png" alt="Ellipse101727" class="cart1440-ellipse10" />
+                <img src="pbl/public6/external/ellipse101727-wzz8-200h.png" alt="Ellipse101727" class="cart1440-ellipse10" />
                 <span class="cart1440-text">1</span>
               </div>
               <span class="cart1440-text01"><span>Корзина</span></span>
@@ -51,19 +51,19 @@ else:
             <div class="cart1440-group339">
               <span class="cart1440-text03"><span>Оформление заказа</span></span>
               <div class="cart1440-group2811">
-                <img src="public6/external/ellipse101727-wzz8-200h.png" alt="Ellipse101727" class="cart1440-ellipse101" />
+                <img src="pbl/public6/external/ellipse101727-wzz8-200h.png" alt="Ellipse101727" class="cart1440-ellipse101" />
                 <span class="cart1440-text05">2</span>
               </div>
             </div>
             <div class="cart1440-group340">
               <span class="cart1440-text06"><span>Заказ принят</span></span>
               <div class="cart1440-group280">
-                <img src="public6/external/ellipse101721-eltn-200h.png" alt="Ellipse101721" class="cart1440-ellipse102" />
+                <img src="pbl/public6/external/ellipse101721-eltn-200h.png" alt="Ellipse101721" class="cart1440-ellipse102" />
                 <span class="cart1440-text08">3</span>
               </div>
             </div>
-            <img src="public6/external/line11626-7jlg.svg" alt="Line11626" class="cart1440-line1" />
-            <img src="public6/external/line21626-6ukp.svg" alt="Line21626" class="cart1440-line2" />
+            <img src="pbl/public6/external/line11626-7jlg.svg" alt="Line11626" class="cart1440-line1" />
+            <img src="pbl/public6/external/line21626-6ukp.svg" alt="Line21626" class="cart1440-line2" />
           </div>
           <div class="cart1440-container1">
             <span class="cart1440-text09"><span>Shayrmen</span></span>
@@ -83,34 +83,34 @@ else:
             <span class="cart1440-text25"><span>Ханты-Мансийск ул. Студенческая 7 </span></span>
             <span class="cart1440-text27"><span>Остались вопросы? А мы всегда на связи: </span></span>
             <span class="cart1440-text29"><span>Написать нам</span></span>
-            <img src="public6/external/rectangle671746-ee0e-200h.png" alt="Rectangle671746" class="cart1440-rectangle67" />
-            <img src="public6/external/rectangle681747-4dmc-200h.png" alt="Rectangle681747" class="cart1440-rectangle68" />
-            <img src="public6/external/rectangle691748-9q4j-200h.png" alt="Rectangle691748" class="cart1440-rectangle69" />
-            <img src="public6/external/rectangle701749-qnyv-200h.png" alt="Rectangle701749" class="cart1440-rectangle70" />
-            <img src="public6/external/rectangle711750-e8gs-200h.png" alt="Rectangle711750" class="cart1440-rectangle71" />
-            <img src="public6/external/rectangle721751-j2vp-200h.png" alt="Rectangle721751" class="cart1440-rectangle72" />
-            <img src="public6/external/rectangle741752-vth-300w.png" alt="Rectangle741752" class="cart1440-rectangle74" />
+            <img src="pbl/public6/external/rectangle671746-ee0e-200h.png" alt="Rectangle671746" class="cart1440-rectangle67" />
+            <img src="pbl/public6/external/rectangle681747-4dmc-200h.png" alt="Rectangle681747" class="cart1440-rectangle68" />
+            <img src="pbl/public6/external/rectangle691748-9q4j-200h.png" alt="Rectangle691748" class="cart1440-rectangle69" />
+            <img src="pbl/public6/external/rectangle701749-qnyv-200h.png" alt="Rectangle701749" class="cart1440-rectangle70" />
+            <img src="pbl/public6/external/rectangle711750-e8gs-200h.png" alt="Rectangle711750" class="cart1440-rectangle71" />
+            <img src="pbl/public6/external/rectangle721751-j2vp-200h.png" alt="Rectangle721751" class="cart1440-rectangle72" />
+            <img src="pbl/public6/external/rectangle741752-vth-300w.png" alt="Rectangle741752" class="cart1440-rectangle74" />
             <span class="cart1440-text31"><span>8 999 999-55-66</span></span>
-            <img src="public6/external/image171754-ehzd-200h.png" alt="image171754" class="cart1440-image17" />
-            <img src="public6/external/image181755-dn5u-200h.png" alt="image181755" class="cart1440-image18" />
-            <img src="public6/external/image211756-mv3b-200h.png" alt="image211756" class="cart1440-image21" />
-            <img src="public6/external/image221757-ra1-200h.png" alt="image221757" class="cart1440-image22" />
-            <img src="public6/external/image191758-tvqk-200h.png" alt="image191758" class="cart1440-image19" />
-            <img src="public6/external/image201759-1c5c-200h.png" alt="image201759" class="cart1440-image20" />
+            <img src="pbl/public6/external/image171754-ehzd-200h.png" alt="image171754" class="cart1440-image17" />
+            <img src="pbl/public6/external/image181755-dn5u-200h.png" alt="image181755" class="cart1440-image18" />
+            <img src="pbl/public6/external/image211756-mv3b-200h.png" alt="image211756" class="cart1440-image21" />
+            <img src="pbl/public6/external/image221757-ra1-200h.png" alt="image221757" class="cart1440-image22" />
+            <img src="pbl/public6/external/image191758-tvqk-200h.png" alt="image191758" class="cart1440-image19" />
+            <img src="pbl/public6/external/image201759-1c5c-200h.png" alt="image201759" class="cart1440-image20" />
             <div class="cart1440-group266">
-              <img src="public6/external/rectangle751761-ts1i.svg" alt="Rectangle751761" class="cart1440-rectangle75" />
+              <img src="pbl/public6/external/rectangle751761-ts1i.svg" alt="Rectangle751761" class="cart1440-rectangle75" />
               <span class="cart1440-text33"><span>Заказать звонок</span></span>
             </div>
             <span class="cart1440-text35"><span>Shayrmen.nyam</span></span>
             <span class="cart1440-text37"><span>Shayrmen.nyam Все права защищены © 2024</span></span>
             <div class="cart1440-group328">
-              <img src="public6/external/image231765-zsj-200h.png" alt="image231765" class="cart1440-image23" />
+              <img src="pbl/public6/external/image231765-zsj-200h.png" alt="image231765" class="cart1440-image23" />
             </div>
             <div class="cart1440-group327">
-              <img src="public6/external/image241768-1sp7-200h.png" alt="image241768" class="cart1440-image24" />
+              <img src="pbl/public6/external/image241768-1sp7-200h.png" alt="image241768" class="cart1440-image24" />
             </div>
             <div class="cart1440-group326">
-              <img src="public6/external/image251771-vzdr-200h.png" alt="image251771" class="cart1440-image25" />
+              <img src="pbl/public6/external/image251771-vzdr-200h.png" alt="image251771" class="cart1440-image25" />
             </div>
           </div>
           <div class="cart1440-group349">
